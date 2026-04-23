b7f9dca7703dca4a1211d7c73fbbfd20233b57b8
marinanthi <marianthikamenidoy@gmail.com>
2026-04-23 16:39:11 +0300

    updated kafka controller, service and new IdempotentConsumer class

--- src/main/java/com/example/onboarding3/controllers/KafkaController.java
+++ src/main/java/com/example/onboarding3/controllers/KafkaController.java
@@ -3,6 +3,7 @@ package com.example.onboarding3.controllers;
 import com.example.onboarding3.domain.KafkaCountries;
 import com.example.onboarding3.services.KafkaService;
 import jakarta.validation.Valid;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -17,18 +18,93 @@ public class KafkaController {
         this.kafkaService = kafkaService;
     }
 
+    //    project endpoint 3
     @PostMapping(path ="/kafka", consumes = "application/json")
     public void sendToKafka(@Valid @RequestBody KafkaCountries country) {
         kafkaService.sendCountry(country);
     }
 
-    @GetMapping( "/kafka/{capitalName}")
-    public List<KafkaCountries> getKafkaCountry(@PathVariable String capitalName) {
+    //    project endpoint 4
+    @GetMapping( "/kafka/{countryName}")
+    public List<KafkaCountries> getKafkaCountry(@PathVariable String countryName) {
         List<KafkaCountries> countries = new ArrayList<>();
-        KafkaCountries ans = kafkaService.getCountry(capitalName);
+        KafkaCountries ans = kafkaService.getCountry(countryName);
         if(ans != null) {
             countries.add(ans);
         }
         return countries;
     }
+
+    @PostMapping(path ="/kafka2", consumes = "application/json")
+    public void send(@Valid @RequestBody KafkaCountries country) throws InterruptedException {
+        kafkaService.sendCountryToKafka(country);
+    }
+
+    //    endpoints to check idempotency and exactly once
+    @GetMapping("/c")
+    public String sth() throws InterruptedException {
+        List<KafkaCountries> d = Arrays.asList(
+                new KafkaCountries("Greece", "Athens", "Europe", "Greek", "Euro", "2"),
+                new KafkaCountries("France", "Paris", "Europe", "French", "Euro", "67"),
+                new KafkaCountries("Germany", "Berlin", "Europe", "German", "Euro", "83"),
+                new KafkaCountries("Italy", "Rome", "Europe", "Italian", "Euro", "60"),
+                new KafkaCountries("Spain", "Madrid", "Europe", "Spanish", "Euro", "47"),
+                new KafkaCountries("Portugal", "Lisbon", "Europe", "Portuguese", "Euro", "10"),
+                new KafkaCountries("Netherlands", "Amsterdam", "Europe", "Dutch", "Euro", "17"),
+                new KafkaCountries("Belgium", "Brussels", "Europe", "Dutch/French", "Euro", "11"),
+                new KafkaCountries("Sweden", "Stockholm", "Europe", "Swedish", "Krona", "10"),
+                new KafkaCountries("Norway", "Oslo", "Europe", "Norwegian", "Krone", "5"),
+                new KafkaCountries("Denmark", "Copenhagen", "Europe", "Danish", "Krone", "6"),
+                new KafkaCountries("Finland", "Helsinki", "Europe", "Finnish", "Euro", "5"),
+                new KafkaCountries("Poland", "Warsaw", "Europe", "Polish", "Zloty", "38"),
+                new KafkaCountries("Austria", "Vienna", "Europe", "German", "Euro", "9"),
+                new KafkaCountries("Switzerland", "Bern", "Europe", "German/French", "Franc", "8"),
+                new KafkaCountries("Czech Republic", "Prague", "Europe", "Czech", "Koruna", "10"),
+                new KafkaCountries("Hungary", "Budapest", "Europe", "Hungarian", "Forint", "9"),
+                new KafkaCountries("Ireland", "Dublin", "Europe", "English", "Euro", "5"),
+                new KafkaCountries("Romania", "Bucharest", "Europe", "Romanian", "Leu", "19")
+        );
+
+//        for(KafkaCountries c : d) {
+//            System.out.println("Sending: " + c);
+//            kafkaService.sendCountryToKafka(c);
+//            System.out.println("Sent: " + c);
+//        }
+//        System.out.println("Done");
+        return kafkaService.forLoop(d);
+    }
+
+    @GetMapping("/c2")
+    public void sth2() throws InterruptedException {
+        List<KafkaCountries> d = Arrays.asList(
+                new KafkaCountries("Greece", "Athens", "Europe", "Greek", "Euro", "2"),
+                new KafkaCountries("France", "Paris", "Europe", "French", "Euro", "67"),
+                new KafkaCountries("Germany", "Berlin", "Europe", "German", "Euro", "83"),
+                new KafkaCountries("Italy", "Rome", "Europe", "Italian", "Euro", "60"),
+                new KafkaCountries("Spain", "Madrid", "Europe", "Spanish", "Euro", "47"),
+                new KafkaCountries("Portugal", "Lisbon", "Europe", "Portuguese", "Euro", "10"),
+                new KafkaCountries("Netherlands", "Amsterdam", "Europe", "Dutch", "Euro", "17"),
+                new KafkaCountries("Belgium", "Brussels", "Europe", "Dutch/French", "Euro", "11"),
+                new KafkaCountries("Sweden", "Stockholm", "Europe", "Swedish", "Krona", "10"),
+                new KafkaCountries("Norway", "Oslo", "Europe", "Norwegian", "Krone", "5"),
+                new KafkaCountries("Denmark", "Copenhagen", "Europe", "Danish", "Krone", "6"),
+                new KafkaCountries("Finland", "Helsinki", "Europe", "Finnish", "Euro", "5"),
+                new KafkaCountries("Poland", "Warsaw", "Europe", "Polish", "Zloty", "38"),
+                new KafkaCountries("Austria", "Vienna", "Europe", "German", "Euro", "9"),
+                new KafkaCountries("Switzerland", "Bern", "Europe", "German/French", "Franc", "8"),
+                new KafkaCountries("Czech Republic", "Prague", "Europe", "Czech", "Koruna", "10"),
+                new KafkaCountries("Hungary", "Budapest", "Europe", "Hungarian", "Forint", "9"),
+                new KafkaCountries("Ireland", "Dublin", "Europe", "English", "Euro", "5"),
+                new KafkaCountries("Romania", "Bucharest", "Europe", "Romanian", "Leu", "19")
+        );
+
+        for(KafkaCountries c : d) {
+            System.out.println("Sending: " + c);
+            kafkaService.sendCountry(c);
+            Thread.sleep(1000);
+            System.out.println("Sent: " + c);
+        }
+        System.out.println("Done");
+    }
+
 }
--- src/main/java/com/example/onboarding3/services/KafkaService.java
+++ src/main/java/com/example/onboarding3/services/KafkaService.java
@@ -2,35 +2,88 @@ package com.example.onboarding3.services;
 
 import com.example.onboarding3.domain.Country;
 import com.example.onboarding3.domain.KafkaCountries;
+import com.example.onboarding3.kafka.IdempotentConsumer;
 import com.example.onboarding3.repositories.KafkaCountriesMongoRepo;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class KafkaService {
 
-    private final KafkaTemplate<String, Country> KafkaTemplate;
+    Logger log = LoggerFactory.getLogger(KafkaService.class.getName());
+    private final KafkaTemplate<String, Country> kafka;
     private final KafkaCountriesMongoRepo kafkaCountriesRepo;
+    private final IdempotentConsumer consumer;
 
-    public KafkaService(KafkaTemplate<String, Country> KafkaTemplate, KafkaCountriesMongoRepo kafkaCountriesRepo) {
-        this.KafkaTemplate = KafkaTemplate;
+    public KafkaService(KafkaTemplate<String, Country> kafka, KafkaCountriesMongoRepo kafkaCountriesRepo, IdempotentConsumer consumer) {
+        this.kafka = kafka;
         this.kafkaCountriesRepo = kafkaCountriesRepo;
+        this.consumer = consumer;
     }
 
+//    first attempt
     @Transactional
     public void sendCountry(KafkaCountries country) {
-        KafkaTemplate.send("countries-topic", country);
-        consumeCountry(country);
+        CompletableFuture<SendResult<String, Country>> future = kafka.send("countries-topic", country);
+        future.whenComplete((result, exception) -> {
+            if (exception != null) {
+                log.error("Error sending message: {}", exception.getMessage());
+            } else {
+                log.info("Message sent successfully. Offset: {}", result.getRecordMetadata().offset());
+
+                System.out.println(result.getProducerRecord().value());
+            }
+        });
     }
 
+    @Transactional(rollbackFor = Exception.class)
     @KafkaListener(topics = "countries-topic", groupId = "country-group")
     public void consumeCountry(KafkaCountries country) {
         kafkaCountriesRepo.save(country);
+        log.info("Received message: {}", country);
     }
 
+//    4th endpoint (onboarding project)
     public KafkaCountries getCountry(String countryName) {
         return kafkaCountriesRepo.findByName(countryName);
     }
+
+//    second attempt
+//    sending to kafka and then consuming from kafka
+    @Transactional(rollbackFor = Exception.class)
+    public void sendCountryToKafka(KafkaCountries country) throws InterruptedException {
+        SendResult<String, Country> res = kafka.send("new-topic", country).join();
+        System.out.println("sent");
+        log.info("Message sent successfully. Offset: {}", res.getRecordMetadata().offset());
+        Thread.sleep(5000);
+        ConsumerRecord<String, Country> record
+                = new ConsumerRecord<String, Country>("countries-topic-2", res.getRecordMetadata().partition(), res.getRecordMetadata().offset(), res.getProducerRecord().key(), res.getProducerRecord().value());
+        consumer.processRecord(record);
+        System.out.println("consumed");
+        Thread.sleep(5000);
+        kafkaCountriesRepo.save(country);
+        System.out.println("saved");
+        Thread.sleep(1000);
+    }
+
+//    testing transactional
+    @Transactional
+    public String forLoop(List<KafkaCountries> d) throws InterruptedException {
+        for(KafkaCountries c : d) {
+            System.out.println("Sending: " + c);
+            sendCountryToKafka(c);
+            System.out.println("Sent: " + c);
+        }
+        return "Done";
+    }
+
 }
