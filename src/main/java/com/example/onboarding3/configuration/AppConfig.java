7b6e468d98f9378c18cae811e1e545ccba6e5d4e
marinanthi <marianthikamenidoy@gmail.com>
2026-04-23 16:57:03 +0300

    config

--- src/main/java/com/example/onboarding3/configuration/AppConfig.java
+++ src/main/java/com/example/onboarding3/configuration/AppConfig.java
@@ -1,9 +1,18 @@
 package com.example.onboarding3.configuration;
 
+import com.example.onboarding3.domain.Country;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -16,6 +25,20 @@ public class AppConfig {
     @Bean
     RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         return RedisCacheManager.builder(connectionFactory)
+                .build();
+    }
+
+    @Bean
+    public KafkaConsumer<String, Country> kafkaConsumer(ConsumerFactory<String, Country> consumerFactory) {
+        return (KafkaConsumer<String, Country>) consumerFactory.createConsumer();
+    }
+
+    @Bean
+    public NewTopic newTopic() {
+        return TopicBuilder.name("new-topic")
+                .partitions(2)
+                .replicas(1)
+                .config("retension.ms", "10000")
                 .build();
     }
 }
